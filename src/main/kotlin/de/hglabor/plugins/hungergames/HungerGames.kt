233aa324aeaa59810a916ac7bfcf761e0712aaad
Tobi van Qoch <75244203+TobivanQoch@users.noreply.github.com>
2022-04-14 17:57:49 +0200

    Update HungerGames.kt

--- src/main/kotlin/de/hglabor/plugins/hungergames/HungerGames.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/HungerGames.kt
@@ -77,6 +77,9 @@ class HungerGames: KSpigot() {
         Bukkit.getOfflinePlayer(UUID.fromString("6bf9509e-d439-4460-ad35-f61cc052baee")).apply { //TOBACKE
             isWhitelisted = true; isOp = true
         }
+        Bukkit.getOfflinePlayer(UUID.fromString("50bf6931-e149-4743-9210-92cd58d85c5d")).apply { //TAITO
+            isWhitelisted = true; isOp = true
+        }
     }
 }
 
